c3de27fc8053a61c3cf001faf0788755165f516b
vitalcha <53018594+vitalcha@users.noreply.github.com>
2019-10-17 15:14:35 +0300

    Add files via upload

--- homework_5/task_1.js
+++ homework_5/task_1.js
@@ -58,37 +58,36 @@
 // getItems() - возвращает массив текущих товаров
 // addItem(item) - получает новый товар и добавляет его к текущим
 // removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    for (let el of items)
+      if (el === item)
+     return this.items.splice(this.items.indexOf(el), 1);
+  }
+}
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
 
-// class Storage {
-//   constructor(items) {
-//     this.items = items;
-//   }
-//   getItems() {
-//     return this.items;
-//   }
-//   addItem(item) {
-//     this.items.push(item);
-//   }
-//   removeItem(item) {
-//     for (let el of item)
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
 
-//     return this.items.splice(this.items.indexOf(el),1);
-//   }
-// }
-// const storage = new Storage([
-//   'Нанитоиды',
-//   'Пролонгер',
-//   'Железные жупи',
-//   'Антигравитатор',
-// ]);
-// const items = storage.getItems();
-// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
-
-// storage.addItem("Дроид");
-// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
-
-// storage.removeItem("Пролонгер");
-// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
 
 // Задание 4
 // Напиши класс StringBuilder. На вход он получает один параметр - строку, которую записывает в свойство _value.
