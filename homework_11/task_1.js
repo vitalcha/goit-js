8023c093661cc79f234417ee7f869f8926d50983
vitalcha <53018594+vitalcha@users.noreply.github.com>
2019-10-28 12:43:48 +0200

    1

---
+++ homework_11/task_1.js
@@ -0,0 +1,59 @@
+// // Задание 1
+
+// //Расставь отсутствующие this в методах объекта account.
+
+// const account = {
+//   owner: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['order-1', 'order-2', 'order-3'],
+//   changeDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost;
+//     orders.push(order);
+//   },
+// };
+
+// account.changeDiscount(0.15);
+// console.log(account.discount); // 0.15
+
+// const orders = account.getOrders();
+// console.log(orders); // ['order-1', 'order-2', 'order-3']
+
+// account.addOrder(5000, 'order-4');
+// console.log(account.balance); // 19000
+// console.log(account.orders); // ['order-1', 'order-2', 'order-3', 'order-4']
+
+// Задание 2
+
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+// const inventory = {
+//   items: ['Монорельса', 'Фильтр'],
+//   add(itemName) {
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function(itemName, action) {
+//   console.log(`Invoking ${action.name} opeartion on ${itemName}`);
+//   action(itemName);
+// };
+
+// invokeInventoryAction('Аптечка', inventory.add.bind(inventory));
+// // Invoking add opeartion on Аптечка
+
+// console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+// invokeInventoryAction('Фильтр', inventory.remove.bind(inventory));
+// // Invoking remove opeartion on Фильтр
+
+// console.log(inventory.items); // ['Монорельса', 'Аптечка']
